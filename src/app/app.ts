f182afb06f1c6187ffb92c8fb2f8fbff4f613ee0
juliodummy2 <juliodummy2@gmail.com>
2026-03-31 19:10:52 -0500

    footer

--- src/app/app.ts
+++ src/app/app.ts
@@ -10,10 +10,11 @@ import { TopTrending } from './top-trending/top-trending';
 import { Reviews } from './reviews/reviews';
 import { Discount } from './discount/discount';
 import { Articles } from './articles/articles';
+import { Footer } from "./footer/footer";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Menu, WhyTourz, Banner, Destinations, PopularTours, BookNow, TopTrending, Reviews, Discount, Articles], 
+  imports: [RouterOutlet, Menu, WhyTourz, Banner, Destinations, PopularTours, BookNow, TopTrending, Reviews, Discount, Articles, Footer], 
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
